2923725a5907b46ff27fc1b72d7e1fcc279cca6b
LailaNovianita <78520440+LailaNovianita@users.noreply.github.com>
2021-02-25 11:16:40 +0700

    Create perulangan_for.dart

---
+++ perulangan_for.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+main(){
+  stdout.write("Jumlah perulangan: ");
+  int n = int.parse(stdin.readLineSync());
+
+  for(int i = 1; i <= n; i++){
+    print("Perulangan ke-$i");
+  }
+}
