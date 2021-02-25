eed6de2b7af8072cefe242f1a1a050d866328a33
LailaNovianita <78520440+LailaNovianita@users.noreply.github.com>
2021-02-25 11:24:47 +0700

    Update and rename list.dart to program_list.dart

--- list.dart
+++
@@ -1,8 +0,0 @@
-// membuat list kosong dengan ukuran 10
-var namaList = new List(10);
-
-// membuat list kosong yang tak jelas ukuran panjangnya
-var namaList = [];
-
-// membuat list sekaligus mengisinya dengan nilai
-var namaList = ["belajar", "dart", "di", "petani kode"];
